9a22de7058ad66014ae6eeaf13b8325ce0af7175
Joshua <joshua@jbna.nl>
2012-01-10 17:25:35 +0100

    Set up git on /datastore/webdb1249/htdocs/

---
+++ index.php
@@ -0,0 +1,20 @@
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+  <head>
+    <title>webdb1249</title>
+  </head>
+  <body>
+    Dit is de <em>Webprogrammeren en Databases</em> pagina voor groep <b>webdb1249</b>.
+    <p />
+    Je bent hier gekomen via het domein:<pre>
+    <?php
+    echo getenv("HTTP_HOST");
+    ?>
+    </pre>
+    Als je een zelf geregistreerd domein wilt laten verwijzen naar deze pagina, dan kan dat
+    <a href="http://websec.science.uva.nl/webdb/registerdomain/">hier</a>.
+  </body>
+</html>
