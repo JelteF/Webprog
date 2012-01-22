afb050461e9bea105a71c57ad75a81d536e769b3
Mike Trieu <mt_tr1pl@hotmail.com>
2012-01-22 14:00:31 +0100

    made search function, dunnow what to do with filter <_<

---
+++ public_html/search.php
@@ -0,0 +1,21 @@
+<?php
+  $taal = $_GET["q"];
+
+  $con = mysql_connect("localhost","webdb1249","uvabookdb");
+  if (!$con)
+    die('could not connect' . mysql.error());
+  mysql_select_db("webdb1249", $con);
+  
+  $result = mysql_query("SELECT id,naam FROM studies WHERE naam LIKE '%$q%' OR faculteit LIKE '%$q%' OR cluster LIKE '%$q%' OR zoekwoorden LIKE '%$q%' ORDER BY naam ASC");
+
+  while($row = mysql_fetch_array($result)) {
+    echo "<a href='study.php?id=";
+    echo $row['id'];
+    echo "'>";
+    echo $row['naam'];
+    echo "</a>";
+    echo "<br />";
+  }
+
+  mysql_close($con);
+?>
