00c1276125d2562f031a5bbdcb851b68fd3bf20b
Jelte Fennema <jelte12345@gmail.com>
2012-01-23 19:12:29 +0100

    tijd aan het fixen

--- public_html/addcomment.php
+++ public_html/addcomment.php
@@ -13,8 +13,8 @@ $post_id = mysql_real_escape_string($_POST['post_id']);
 $user_id = "10183159";
 mysql_select_db("webdb1249", $con);
 mysql_query("UPDATE posts SET studie='$studie' WHERE ID='$post_id'");
-$row = mysql_fetch_array(mysql_query("SELECT * FROM posts WHERE ID=$post_id"));
-echo $row['content'];
+$tijd = mysql_fetch_object(mysql_query("SELECT tijd FROM posts WHERE ID=$post_id"));
+echo $tijd;
 
 echo" <div class='row'>
   <div class='span2'>
