5ca598a1f11697fa813b198168b44bb37ad39dc8
Jelte Fennema <jelte12345@gmail.com>
2012-01-23 13:45:06 +0100

    punt toegevoegd in extension bij upload

--- public_html/ajaxphp/post.php
+++ public_html/ajaxphp/post.php
@@ -18,7 +18,7 @@
                 $destination_path = "/datastore/webdb1249/Webprog/public_html/";
                 $result = "1";
                 $extension = basename($_FILES['file']['name']);
-                $extension = file_extension($extension);
+                $extension = ".".file_extension($extension);
                 mysql_select_db("webdb1249", $con);
                 mysql_query("INSERT INTO uploads (file) VALUES ('')");
                 $filename = mysql_insert_id().$extension;
