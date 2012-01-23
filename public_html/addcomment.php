7f23e550fcbb3ede145195d485fb8e90713d3d7b
Jelte Fennema <jelte12345@gmail.com>
2012-01-23 18:41:30 +0100

    fixing stuff

--- public_html/addcomment.php
+++ public_html/addcomment.php
@@ -13,6 +13,8 @@ $post_id = mysql_real_escape_string($_POST['post_id']);
 $user_id = "10183159";
 mysql_select_db("webdb1249", $con);
 mysql_query("UPDATE posts SET studie='$studie' WHERE ID='$post_id'");
+$tijd= mysql_query("SELECT tijd FROM posts WHERE ID='$posts");
+echo $tijd;
 
 echo" <div class='row'>
   <div class='span2'>
