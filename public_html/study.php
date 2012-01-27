9398704f4447b647bc5b37e76750c762bb696912
Jelte Fennema <jelte12345@gmail.com>
2012-01-27 17:38:00 +0100

    form alleen accessible wanneer ingelogd

--- public_html/study.php
+++ public_html/study.php
@@ -95,7 +95,7 @@ foreach ($paragraphs as $paragraph) {
                                 <h3>Reageer op deze opleiding!</h3>
                                 <div class="input">
                                     <form name="postForm"  action="ajaxphp/post.php" method="post" enctype="multipart/form-data" target="post_target" class="pull-left" onsubmit="startUpload();">
-                                        <input name="naam" class="medium" type="text" placeholder="Naam" <?php if(!$validated) echo "disabled"; else echo "value='".$user['naam']."'";?> />
+                                        <input name="naam" class="medium" type="text" placeholder="Naam" <?php if(!$validated) echo "disabled"; else echo "value='".$user1['naam']."'";?> />
                                         <p></p>
                                         Post:
                                         <input id="img" type="radio" value="img" name="post-type" checked <?php if(!$validated) echo "disabled";?> onclick="contentselect('img') ;"/>Image
