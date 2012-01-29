78267c5983c7690ddddc38659407896e6b3a7a81
Jelte Fennema <jelte12345@gmail.com>
2012-01-30 00:36:12 +0100

    joshuas like gedeelte

--- public_html/ajaxphp/like.php
+++ public_html/ajaxphp/like.php
@@ -13,23 +13,24 @@ $result = mysql_query("SELECT * FROM users WHERE ticket='$ticket'");
 $array = mysql_fetch_array($result);
 $user_id = $array['id'];
 
-if ($up)
-  $newvalue = 1;
-else
-  $newvalue = -1;
+if ($up) $newvalue = 1;
+else $newvalue = -1;
+
+echo $up . $newvalue
 
 $result = mysql_query("SELECT * FROM votes WHERE voter='$user_id', post='$post'");
 if ($result && $array  = mysql_fetch_array($result)) {
   $value = $array['vote'];
-  if ($value != 1 && $up)
-    $newvalue = 1;
-  elseif ($newvalue != -1 && !$up)
-    $newvalue = -1;
+
+  if ($value == $newvalue)
+    $newvalue = 0;
+  else
+    $newvalue *= -1;
+
   mysql_query("UPDATE votes SET vote='$newvalue' WHERE voter='$user_id', post='$post'");
 }
 else
   mysql_query("INSERT INTO votes (voter, post, vote) VALUES ('$user_id', '$post', '$newvalue')");
-
 
 if ($newvalue != 0)
   mysql_query("UPDATE posts SET score=score+$newvalue, score_week=score_week+$newvalue WHERE id='$post'");
