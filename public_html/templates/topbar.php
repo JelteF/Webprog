e434b400b3e9e84622dac5132b0b9dc3ad0ea5de
Mike Trieu <mt_tr1pl@hotmail.com>
2012-01-31 00:31:10 +0100

    topbarsearch reverted, dunnow why onblur doesnt work

--- public_html/templates/topbar.php
+++ public_html/templates/topbar.php
@@ -22,10 +22,6 @@
     xmlhttp.open("GET","ajaxphp/topbarsearch.php?q="+str,true);
     xmlhttp.send();
   }
-  function clear() {
-    var x = document.getElementById("forclearing");
-    x.value = "";
-  }
 </script>
 <?php $page_name = $_SERVER['PHP_SELF'];
 require("../servercode/connect.php");
@@ -47,7 +43,7 @@ require("../servercode/connect.php");
             <li><a href='#contact'>Contact</a></li>
           </ul>
           <form class="pull-left" action="opleidingen.php" method="GET">
-            <input class="input-large" type="text" id="forclearing" name="search" placeholder="Zoek een opleiding" onkeyup="topresult(this.value)" onblur="clear()" autocomplete="off" />
+            <input class="input-large" type="text" id="forclearing" name="search" placeholder="Zoek een opleiding" onkeyup="topresult(this.value)" autocomplete="off" />
             <div id="topbarsearch" style="z-index:10"></div>
           </form>
           <?php require("ajaxphp/login.php")?>
