b6c96c032fb8218ce783e540a2f1c10c2fe76887
Mike Trieu <mt_tr1pl@hotmail.com>
2012-01-30 23:24:06 +0100

    search improvements

--- public_html/ajaxphp/topbarsearch.php
+++ public_html/ajaxphp/topbarsearch.php
@@ -3,7 +3,11 @@ $con = mysql_connect("localhost","webdb1249","uvabookdb");
 if (!$con)
   die('could not connect' . mysql.error());
 
-$q = mysql_real_escape_string($_GET["q"]);
+if(isset($_GET["q"])) {
+  $q = mysql_real_escape_string($_GET["q"]);
+} else {
+  $q = "";
+}
 
 mysql_select_db("webdb1249", $con);
 
@@ -11,6 +15,7 @@ $result = mysql_query("SELECT id,naam FROM studies WHERE naam LIKE '%$q%' ORDER 
 
 if(mysql_num_rows($result)==0) {
   echo "Geen resultaat";
+    echo "<b><a href='opleidingen.php?search=$q'>Naar zoekpagina</a></b>";
 } else {
   while($row = mysql_fetch_array($result)) {
     echo "<a href='study.php?id=";
@@ -20,6 +25,7 @@ if(mysql_num_rows($result)==0) {
     echo "</a>";
     echo "<br />";
   }
+  echo "<b><a href='opleidingen.php?search=$q'>Meer</a></b>";
 }
 
 mysql_close($con);
