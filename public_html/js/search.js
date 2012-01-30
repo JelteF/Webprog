d9995216bd24d647094157f9587176778253438a
Jelte Fennema <jelte12345@gmail.com>
2012-01-30 14:47:03 +0100

    search id shit

--- public_html/js/search.js
+++ public_html/js/search.js
@@ -1,26 +1,26 @@
 function result() {
-str = document.getElementById("srchblok").value;
-  taal = 0;
+var str = document.getElementById("srchblok").value;
+  var taal = 0;
   for(taal = 0; taal < 3; taal++) {
       if (document.getElementById("taal"+taal).checked)
           break;
   }
-  titel = 0;
+  var titel = 0;
   for(titel = 0; titel < 10; titel++) {
       if (document.getElementById("titel"+titel).checked)
           break;
   }
-  studievorm = 0;
+  var studievorm = 0;
   for(studievorm = 0; studievorm < 4; studievorm++) {
       if (document.getElementById("studievorm"+studievorm).checked)
           break;
   }
-  intr = 0;
+  var intr = 0;
   for(intr = 0; intr < 14; intr++) {
       if (document.getElementById("intr"+intr).checked)
           break;
   }
-  fac = 0;
+  var fac = 0;
   for(fac = 0; fac < 8; fac++) {
       if (document.getElementById("fac"+fac).checked)
           break;
