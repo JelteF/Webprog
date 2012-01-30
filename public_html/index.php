86a1aef0da1a4a35bd69a64be80fddf29472250d
Alper Yerlibucak <alper.yerlibucak@gmail.com>
2012-01-30 05:03:41 -0800

    homepage update

--- public_html/index.php
+++ public_html/index.php
@@ -12,8 +12,14 @@
     <!---container-->
     <div class="container">
       <div class="content">
-	<h1>what's new</h1>
-	<?php require("ajaxphp/homepage_posts.php") ?>
+		<div class="welkom">
+			<h1>Welkom!</h1>
+			<p>UvAbook geeft de gelegenheid aan studenten en docenten, info te delen over studies door middel van foto's, video's, pdf en text bestanden. Op deze manier geven wij gebruikers een beeld van hoe het er op een bepaalde studie aan toe gaat. De beleving van opendagen en meeloopdagen komt bij jou thuis op je scherm!</p>
+		</div> 
+		<div class="whatsnew">
+			<h1>What's new</h1>
+			<?php require("ajaxphp/homepage_posts.php") ?>
+		</div>
         <div class="row">
           <div class="span16">
           </div>
