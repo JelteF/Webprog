3e078f0c6bb3c7fceb763a22d0608fef89467620
Mike Trieu <trieu@websec.science.uva.nl>
2012-01-31 22:09:21 +0100

    added footer from Joshua to github

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,5 +1,21 @@
     <div class="footer" style="text-align: center">
       <a name='contact'></a>
+      <div class="row" style="margin-left:auto;margin-right:auto;width:900px;">
+        <div class="span6">
+          <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
+          <p>Voor contact email admin@uvabook.nl</p>
+        </div>
+
+        <div class="span6 offset2" style="text-allign:left;">
+        <ul>
+          <li>Joshua Appelman</li>
+          <li>Jelte Fennema</li>
+          <li>Freddy de Greef</li>
+          <li>Mike Trieu</li>
+          <li>Alper Yerlibucak</li>
+        </ul>
+        </div>
+      </div>
       <p>&copy; Universiteit van Amsterdam 2012</p>
     </div>
 <?php
