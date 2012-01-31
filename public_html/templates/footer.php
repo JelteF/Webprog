d0ea6b10565fb6761222b0a255a80c51d8d5762e
Mike Trieu <mt_tr1pl@hotmail.com>
2012-01-31 22:18:14 +0100

    pimping footer.php

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,12 +1,12 @@
     <div class="footer" style="text-align: center">
       <a name='contact'></a>
-      <div class="row" style="margin-left:auto;margin-right:auto;width:900px;">
-        <div class="span6">
+      <div class="row">
+        <div class="contact">
           <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
           <p>Voor contact email admin@uvabook.nl</p>
         </div>
 
-        <div class="span6 offset2" style="text-allign:left;">
+        <div class="contact">
         <ul>
           <li>Joshua Appelman</li>
           <li>Jelte Fennema</li>
