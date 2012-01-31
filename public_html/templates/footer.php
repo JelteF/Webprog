d82add5495263b2c54dbf334ff2f808ef2f3d6f2
Mike Trieu <mt_tr1pl@hotmail.com>
2012-01-31 22:21:10 +0100

    pimping footer.php

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,19 +1,22 @@
     <div class="footer" style="text-align: center">
       <a name='contact'></a>
       <div class="row">
-        <div class="contact">
-          <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
-          <p>Voor contact email admin@uvabook.nl</p>
+        <div class="span8">
+          <div class="contact">
+            <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
+            <p>Voor contact email admin@uvabook.nl</p>
+          </div>
         </div>
-
-        <div class="contact">
-        <ul>
-          <li>Joshua Appelman</li>
-          <li>Jelte Fennema</li>
-          <li>Freddy de Greef</li>
-          <li>Mike Trieu</li>
-          <li>Alper Yerlibucak</li>
-        </ul>
+        <div class="span8">
+          <div class="contact">
+            <ul>
+              <li>Joshua Appelman</li>
+              <li>Jelte Fennema</li>
+              <li>Freddy de Greef</li>
+              <li>Mike Trieu</li>
+              <li>Alper Yerlibucak</li>
+            </ul>
+          </div>
         </div>
       </div>
       <p>&copy; Universiteit van Amsterdam 2012</p>
