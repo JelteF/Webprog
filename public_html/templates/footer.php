63bd7d511d101ac9792e4ac3eb2f300748e45f37
Mike Trieu <mt_tr1pl@hotmail.com>
2012-02-01 23:08:43 +0100

    commented footer.php

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,32 +1,36 @@
-    <div class="container">
-      <a name='contact'></a>
-     <div class="contactblok">
-      <div class="row">
-        <div class="span8">
-          <div class="contact">
-            <h3>UvAbook:</h3>
-            <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
-            <p>Voor contact email admin@uvabook.nl</p>
-          </div>
-        </div>
-        <div class="span8">
-          <div class="contact">
-            <h3>Founded by:</h3>
-            <ul>
-              <li><a href="#">Joshua Appelman</a></li>
-              <li><a href="#">Jelte Fennema</a></li>
-              <li><a href="#">Freddy de Greef</a></li>
-              <li><a href="#">Mike Trieu</a></li>
-              <li><a href="http://www.facebook.com/profile.php?id=100001876938590" target="_blank">Alper Yerlibucak</a></li>
-            </ul>
-          </div>
-        </div>
+<!--
+  Deze pagina wordt gerequired in index.php,opleidingen.php en study.php
+  Bevat contact en auteur informatie
+    Een anchor is gemaakt naar deze informatie. Een link naar de anchor zit in topbar.php
+  Bevat copyright claim
+-->
+<div class="container">
+  <a name='contact'></a>
+  <div class="contactblok">
+    <div class="row">
+      <div class="span8">
+        <div class="contact">
+          <h3>UvAbook:</h3>
+          <p>UvAbook is voor het project Webdevelopment en Databases and de Universiteit van Amsterdam.</p>
+          <p>Voor contact email admin@uvabook.nl</p>
         </div>
       </div>
-    <div class="footer">
-      <p>&copy; Universiteit van Amsterdam 2012</p>
+      <div class="span8">
+        <div class="contact">
+          <h3>Founded by:</h3>
+            <ul>
+              <li><a href="">Joshua Appelman</a></li>
+              <li><a href="">Jelte Fennema</a></li>
+              <li><a href="">Freddy de Greef</a></li>
+              <li><a href="">Mike Trieu</a></li>
+              <li><a href="http://www.facebook.com/profile.php?id=100001876938590" target="_blank">Alper Yerlibucak</a></li>
+            </ul>
+        </div>
+      </div>
     </div>
-    </div>
-<?php
-require("closecon.php");
-?>
+  </div>
+  <div class="footer">
+    <p>&copy; Universiteit van Amsterdam 2012</p>
+  </div>
+</div>
+<?php require("closecon.php"); ?>
