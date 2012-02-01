304e60bcd1368530a706c8de6bc4faf2518469cc
Jelte Fennema <jelte12345@gmail.com>
2012-02-01 14:48:13 +0100

    mergemit

--- public_html/js/search.js
+++ public_html/js/search.js
@@ -30,6 +30,10 @@ var str = document.getElementById("srchblok").value;
   if(window.XMLHttpRequest) {
     xmlhttp = new XMLHttpRequest();
   }
+  else
+  {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
   xmlhttp.onreadystatechange = function() {
     if(xmlhttp.readyState==4 && xmlhttp.status==200) {
       document.getElementById("searchResult").innerHTML=xmlhttp.responseText;
--- public_html/templates/topbar.php
+++ public_html/templates/topbar.php
@@ -9,6 +9,10 @@
     if(window.XMLHttpRequest) {
       xmlhttp = new XMLHttpRequest();
     }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
     xmlhttp.onreadystatechange = function() {
       if(xmlhttp.readyState==4 && xmlhttp.status==200) {
         document.getElementById("topbarsearch").innerHTML=xmlhttp.responseText;
