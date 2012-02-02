9bfad231458abefb1c6ae0316a332a5c5449197c
Jelte Fennema <jelte12345@gmail.com>
2012-02-02 17:12:44 +0100

    comments toegevoegd

--- public_html/ajaxphp/login.php
+++ public_html/ajaxphp/login.php
@@ -1,5 +1,8 @@
 <?php
 echo "<div class='pull-right'>";
+/*
+ * verkrijg de url om terug gestuurd te kunnen worden door CAS
+ */
 $pageURL = 'http://';
 if ($_SERVER["SERVER_PORT"] != "80") {
   $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
@@ -9,15 +12,21 @@ if ($_SERVER["SERVER_PORT"] != "80") {
 $pageURL = explode("?ticket", $pageURL);
 $pageURL = $pageURL[0];
 
-$con = mysql_connect("localhost","webdb1249","uvabookdb") or die(mysql_error());
-mysql_select_db("webdb1249", $con) or die("Database not available");
+require("../../servercode/connect.php");
 
 $validated = false;
 if(isset($_GET["ticket"])) {
-  //user just logged in, validate and store
+  /*
+   * Er staat een ticket in de URL, die moet worden gevalideerd
+   */
   $ticket= $_GET["ticket"];
   $file = file_get_contents("https://bt-lap.ic.uva.nl/cas/serviceValidate?ticket=$ticket&service=$pageURL");
   if(stripos($file, "<cas:authenticationFailure") === false){
+      /*
+       * Het is een geldig ticket dus de user moet een session krijgen.
+       * Als hij nog niet eerder heeft ingelogd moet de user een nieuw account
+       * krijgen, zo niet moet het ticket bij het oude account geupdate worden.
+       */
       $_SESSION['ticket'] = $ticket;
       $startUser = stripos($file,"<cas:user>") + 10;
       $endUser = stripos($file,"</cas:user>");
@@ -34,12 +43,15 @@ if(isset($_GET["ticket"])) {
   }
 }
 if ((isset($_SESSION['ticket']) || $validated)&& (!isset($_GET["do"]) || !$_GET["do"]=="logoff")) {
-  //user is logged in
+  /*
+   * Er is een session gestart, dus de user bestaat en hij wil niet uitloggen.
+   */
   $ticket = $_SESSION['ticket'];
   $result = mysql_query("SELECT * FROM users WHERE ticket='$ticket'");
   if ($result) $rows = mysql_num_rows($result);
   if (isset($rows) && $rows != 0){
     $user1 = mysql_fetch_array($result);
+    //de inlog knop moet veranderd worden in een log uit knop.
     if(strpos($pageURL, "?") !== false)
         echo "<a class='brand' href='".$pageURL."&do=logoff'>Log uit (".$user1['UvAnetID'].")</a>";
     else
@@ -48,13 +60,15 @@ if ((isset($_SESSION['ticket']) || $validated)&& (!isset($_GET["do"]) || !$_GET[
   }
 }
 else {
-  //user is not logged in
+  //de user is niet ingelogd dus moet hij naar cas gestuurd worden om in te loggen
   echo "<a class='brand' href='https://bt-lap.ic.uva.nl/cas/login?service=$pageURL'>Log In</a>";
 }
 
 if(isset($_GET["do"]) && $_GET["do"]=="logoff"){
+    //de user wil uitloggen dus moet de session verwijderd worden.
     unset($_SESSION["ticket"]);
     $_GET['do'] = '';
+    unset($_GET['do']);
     session_unset();
     session_destroy();
 }
